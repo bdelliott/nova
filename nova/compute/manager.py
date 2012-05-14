a2d76ca602779ed360b997192b40d584caa7b352
Rick Harris <rconradharris@gmail.com>
2012-05-14 20:18:53 +0000

    Adding encrypted password to notification.
    This patch will encrypt the instances root password using a supplied
    public key and pass that to a service via the start instance
    notification.  Only add encrypted password on role match.
    
    Change-Id: Id06141ffe1b2644fa7bf87d6c969130573803ee3

--- nova/compute/manager.py
+++ nova/compute/manager.py
@@ -99,6 +99,12 @@ compute_opts = [
                 default=False,
                 help='Whether to start guests that were running before the '
                      'host rebooted'),
+    cfg.ListOpt('password_aware_services',
+                default=[],
+                help='Public keys for services that will receive access to the'
+                     ' instance root password. Format is:'
+                     ' <target>:<key_version>:<path-to-public-key-file> ('
+                     ' Ex. "rack_connect:123:/tmp/rack_connect_123.pub")'),
     ]
 
 interval_opts = [
@@ -663,6 +669,40 @@ class ComputeManager(manager.SchedulerDependentManager):
 
         return block_device_info
 
+    def _get_password_info(self, context, admin_password):
+        """Create encrypted password info that will be passed to specified
+        services via notifications.
+
+        The admin_password is encrypted using a public key supplied for each
+        service. Since the keys for a service may need to change, a version
+        number is also passed so the receiver knows which private key to use to
+        decrypt.
+        """
+        password_info = []
+        for service in CONF.password_aware_services:
+            target, key_version, public_key_filename = service.split(':')
+
+            if target not in context.roles:
+                continue
+
+            with open(public_key_filename, 'r') as f:
+                public_key = f.read()
+
+            # Create byte-stream
+            plain_text = admin_password.encode('utf8')
+
+            # Encrypt using service's public key
+            encrypted_password = utils.encrypt_rsa(
+                public_key, plain_text)
+
+            # Base64 encode to make results transport friendly
+            encrypted_password = encrypted_password.encode('base64')
+
+            password_info.append(dict(target=target,
+                                      key_version=key_version,
+                                      value=encrypted_password))
+        return password_info
+
     def _run_instance(self, context, request_spec,
                       filter_properties, requested_networks, injected_files,
                       admin_password, is_first_time, node, instance):
@@ -684,6 +724,11 @@ class ComputeManager(manager.SchedulerDependentManager):
                 extra_usage_info = {}
 
             self._start_building(context, instance)
+
+            if admin_password:
+                password_info = self._get_password_info(
+                        context, admin_password)
+                extra_usage_info['password_info'] = password_info
 
             self._notify_about_instance_usage(
                     context, instance, "create.start",
@@ -1359,6 +1404,13 @@ class ComputeManager(manager.SchedulerDependentManager):
 
             # This message should contain the new image_ref
             extra_usage_info = {'image_name': image_meta.get('name', '')}
+
+            admin_password = new_pass
+
+            # Get encrypted password for notifications
+            password_info = self._get_password_info(context, admin_password)
+            extra_usage_info['password_info'] = password_info
+
             self._notify_about_instance_usage(context, instance,
                     "rebuild.start", extra_usage_info=extra_usage_info)
 
--- nova/utils.py
+++ nova/utils.py
@@ -38,6 +38,7 @@ import tempfile
 import time
 from xml.sax import saxutils
 
+from Crypto.PublicKey import RSA
 from eventlet import event
 from eventlet.green import subprocess
 from eventlet import greenthread
@@ -1284,3 +1285,13 @@ def metadata_to_dict(metadata):
         if not item.get('deleted'):
             result[item['key']] = item['value']
     return result
+
+
+def encrypt_rsa(public_key, plain_text):
+    if 'importKey' not in dir(RSA):
+        # PyCrypto2.1 compat
+        import nova.compat.pycrypto21
+        nova.compat.pycrypto21.monkey_patch()
+
+    # NOTE(sirp): K-param is not used by RSA
+    return RSA.importKey(public_key).encrypt(plain_text, None)[0]
