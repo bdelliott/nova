4b67abe13381c7b478b5ae1999626744c0dec560
Brian Elliott <brian.elliott@rackspace.com>
2013-03-04 20:33:50 +0000

    Add ability to control max utilization of a cell
    Add a 'reserve_percent' config flag to cells to control the
    maximum free capacity that a child cell will be advertised to its
    parent(s).
    
    This gives the ability to leave some reserve capacity
    in child cells for operational maintenance.
    
    Change-Id: I1dae1eae4f61ca8079c6f24e60176c31fd8a759d

--- nova/cells/opts.py
+++ nova/cells/opts.py
@@ -39,6 +39,9 @@ cells_opts = [
     cfg.IntOpt('call_timeout',
                 default=60,
                 help='Seconds to wait for response from a call to a cell.'),
+    cfg.FloatOpt('reserve_percent',
+                default=10.0,
+                help='Percentage of cell capacity to hold in reserve.'),
 ]
 
 cfg.CONF.register_opts(cells_opts, group='cells')
